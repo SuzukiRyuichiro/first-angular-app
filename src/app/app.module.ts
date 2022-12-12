a9478b889afbb0208b6213754e7d6637da0a10c0
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-12-12 21:28:52 +0100

    add some more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { TaskComponent } from './task/task.component';
+import { MatInputModule } from '@angular/material/input';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 @NgModule({
-  declarations: [AppComponent, TaskListComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, TaskListComponent, TaskComponent, TaskFormComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -9,6 +9,23 @@ export class TaskListComponent {
   tasks = [
     {
       name: 'Task 1',
+      description: 'Vitae commodo sagittis duis',
+      done: false,
+    },
+    {
+      name: 'Task 2',
+      description: 'Vitae commodo sagittis duis',
+      done: false,
+    },
+    {
+      name: 'Task 3',
+      description: 'Vitae commodo sagittis duis',
+      done: false,
+    },
+    {
+      name: 'Task 4',
+      description: 'Vitae commodo sagittis duis',
+      done: false,
     },
   ];
 }
